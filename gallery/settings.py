909f761bfaeb27080ab6b0472620ffc43c86b9e4
kayitesijackie <bazimyas@gmail.com>
2019-03-13 12:41:20 +0200

    add 'bootstrap3'

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photo.apps.PhotoConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
