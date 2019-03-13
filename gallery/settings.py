cff83de81ad3ecf306e3d837ec6250b5cb8d4fc1
kayitesijackie <bazimyas@gmail.com>
2019-03-13 12:34:29 +0200

    change the contents of database by adding username and password

--- gallery/settings.py
+++ gallery/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'saphani',
+        'PASSWORD':'123abc',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
 
 USE_I18N = True
 
