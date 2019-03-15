7c5ec24286a800b7a14cbd31b3a0f6fe9a43da11
kayitesijackie <bazimyas@gmail.com>
2019-03-15 12:22:17 +0200

    import render,redirect, class models and Http404

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Image,Category,Location
+from django.http import Http404
 
 # Create your views here.
