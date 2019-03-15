6936545da0c881da60beef6b52e47ca50b3085ee
kayitesijackie <bazimyas@gmail.com>
2019-03-15 12:23:47 +0200

    add the view of homepage

--- photo/views.py
+++ photo/views.py
@@ -3,3 +3,8 @@ from .models import Image,Category,Location
 from django.http import Http404
 
 # Create your views here.
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'index.html',{'images':images,'locations':locations})
+
