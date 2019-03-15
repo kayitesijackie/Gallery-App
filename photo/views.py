41f0b4a079bf43bc5ff1edc6a6ed74cee618acf0
kayitesijackie <bazimyas@gmail.com>
2019-03-15 12:24:30 +0200

    add the view of location page

--- photo/views.py
+++ photo/views.py
@@ -8,3 +8,13 @@ def index(request):
     locations = Location.objects.all()
     return render(request,'index.html',{'images':images,'locations':locations})
 
+def display_location(request,location_id):
+    try:
+        locations = Location.objects.all()
+        location = Location.objects.get(id = location_id)
+        images = Image.objects.filter(image_location = location.id)
+    except:
+        raise Http404()
+    return render(request,'location.html',{'location':location,'images':images,'locations':locations})
+
+
