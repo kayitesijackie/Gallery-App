487587145c3c31c7c37b8674643a67e7a71fd4f7
kayitesijackie <bazimyas@gmail.com>
2019-03-15 12:15:50 +0200

    add class Location

--- photo/models.py
+++ photo/models.py
@@ -17,3 +17,20 @@ class Category(models.Model):
     def update_category(self, **kwargs):
         self.objects.filter(id = self.pk).update(**kwargs)
 
+class Location(models.Model):
+    name = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def delete_location(cls,name):
+        cls.objects.filter(name = name).delete()
+
+   
+    def update_location(self, **kwargs):
+        self.objects.filter(id = self.pk).update(**kwargs)   
+
