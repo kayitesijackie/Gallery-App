14856df071a216b5ec061b334ebabfc0115982aa
kayitesijackie <bazimyas@gmail.com>
2019-03-15 12:29:09 +0200

    add the view of search page

--- photo/views.py
+++ photo/views.py
@@ -10,11 +10,21 @@ def index(request):
 
 def display_location(request,location_id):
     try:
-        locations = Location.objects.all()
         location = Location.objects.get(id = location_id)
         images = Image.objects.filter(image_location = location.id)
     except:
         raise Http404()
-    return render(request,'location.html',{'location':location,'images':images,'locations':locations})
+    return render(request,'location.html',{'location':location,'images':images})
 
 
+def search_category(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET['category']:
+        search_term = (request.GET.get('category')).title()
+        searched_images = Image.search_by_category(search_term)
+        message = f'{search_term}'
+        return render(request,'search.html',{'message':message,'images':searched_images,'locations':locations})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request,'search.html',{'message':message,'locations':locations})
