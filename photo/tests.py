3afa09fa9274306d502f09cfc7892147abd6bcdd
kayitesijackie <bazimyas@gmail.com>
2019-03-15 12:19:56 +0200

    add ImageTestCase

--- photo/tests.py
+++ photo/tests.py
@@ -43,3 +43,24 @@ class LocationTestCase(TestCase):
         locations = Location.objects.all()
         self.assertTrue(len(locations) == 0)
 
+class ImageTestCase(TestCase):
+    def setUp(self):
+        # Creating a new category and saving it
+        self.new_category = Category(name = 'Food')
+        self.new_category.save_category()
+
+        # Creat a new location and saving it
+        self.new_location = Location(name = 'Kigali')
+        self.new_location.save_location()
+
+        self.new_image = Image(image_name = 'Test Image',image_description = 'Test Description',image_category = self.new_category,image_location = self.new_location)
+        self.new_image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_save_image(self):
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
